fb1b38e291f36b162a15b527c585e661cb3be528
aleckeller <alec.keller.17@gmail.com>
2018-07-27 13:45:20 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,3 +6,16 @@ HTTPCHECKS = ["check-api-umbrella-status-prod","check-chef-cluster","check_http_
 "check_http_sysdig","check_http_vault","check_http_beta.sam.gov","check_http_alpha.sam.gov","check_http_UCP-replica1-prod-check",
 "check_http_UCP-replica2-prod-check","check_http_UCP-ctlr-prod-check","check-openidm-health",
 "check-es-cluster-nonprod","check_docker dtr-","check-api-umbrella-status-non-prod"]
+
+
+#***********EXCEL CONSTANTS***************
+inital_date_cell = 'B11'
+http_check_row = 8
+init_http_check_col = 4
+last_index_col = 184
+dates_col = 2
+init_dates_row = 11
+last_index_row = 443
+time_increment_row = 2
+init_outage_type_col = 8
+init_outage_type_row = 13
